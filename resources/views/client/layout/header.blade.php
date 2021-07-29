470a40577e56653f3928ddc8b7de664652ecf89a
hinhnxth2009027 <nguyenhjnh2002@gmail.com>
2021-07-29 17:35:27 +0700

    init project

---
+++ resources/views/client/layout/header.blade.php
@@ -0,0 +1,16 @@
+<header class="item header margin-top-0">
+    <div class="wrapper">
+        @include('.client.layout.navbar')
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="text-pageheader">
+                        <div class="subtext-image" data-scrollreveal="enter bottom over 1.7s after 0.1s">
+                            Nowi Tinhf yeeu baast ddaafu
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
